eafaf368c408da9b1f315340a19b60061a69e922
Henrique Chaves <canaldomonza@gmail.com>
2023-02-17 23:21:26 -0300

    feat: add new method getTitle getYear and channalGenders

--- script.go
+++ script.go
@@ -3,17 +3,19 @@ package main
 import (
 	"bytes"
 	"encoding/csv"
-	"fmt"
+	"errors"
 	"io"
 	"io/ioutil"
 	"log"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 type Movie struct {
 	ID    int32
 	Title string
+	Year  int
 }
 
 func main() {
@@ -23,36 +25,101 @@ func main() {
 func remountCSV(file string) {
 	body, err := ioutil.ReadFile(file)
 	if err != nil {
-	}
-
-	rx, err := regexp.Compile(`[\\"\\"\/]`)
-	if err != nil {
 		log.Fatal(err.Error())
 	}
-
-	newFile := rx.ReplaceAll(body, []byte(""))
-	readNewFile := csv.NewReader(bytes.NewBuffer(newFile))
+	readNewFile := csv.NewReader(bytes.NewBuffer(body))
+	readNewFile.LazyQuotes = true
 	_, err = readNewFile.Read()
 	if err != nil {
 		log.Fatalln(err.Error())
 	}
-	var movies []Movie
 	for {
 		row, err := readNewFile.Read()
 		if err == io.EOF {
 			break
 		}
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		idClean := removeWhiteSpace(row[0])
+		titleClean := removeWhiteSpace(row[1])
+		genderClean := removeWhiteSpace(row[2])
 
-		id, err := strconv.Atoi(row[0])
+		title := getTitle(titleClean)
+		year, err := getYear(titleClean)
 		if err != nil {
 			log.Fatal(err.Error())
 		}
 
-		movies = append(movies, Movie{
-			ID:    int32(id),
-			Title: row[1],
-		})
+		log.Printf("id: %v", idClean)
+		log.Printf("title: %v", title)
+		log.Printf("year: %v", year)
 
-		fmt.Printf("id: %v, title: %v\n", id, row[1])
+		genders, err := getAllGenders(genderClean)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		genderChannal := make(chan string)
+		go channalOfGenders(genders, genderChannal)
+		<-genderChannal
 	}
+
+}
+
+func removeWhiteSpace(line string) string {
+	rx, err := regexp.Compile(`^[\s]+|[\s]+$`)
+	if err != nil {
+		return ""
+	}
+	stringClean := rx.ReplaceAllString(line, "")
+	return stringClean
+}
+
+func getTitle(line string) string {
+	rx, err := regexp.Compile(`\s*?[\(]{1}[0-9]{4}[\)]{1}`)
+	if err != nil {
+		return ""
+	}
+	newString := rx.ReplaceAllString(line, "")
+	return newString
+}
+func getYear(line string) (int, error) {
+	if line == "" {
+		return 0, errors.New("string line is empty")
+	}
+	rx, err := regexp.Compile(`[^0-9]`)
+	if err != nil {
+		return 0, err
+	}
+	newString := rx.ReplaceAllString(line, "")
+	if newString == "" {
+		return 0, nil
+	}
+	stringToInt, err := strconv.Atoi(newString)
+	if err != nil {
+		return 0, err
+	}
+	return stringToInt, nil
+}
+
+func getAllGenders(line string) ([]string, error) {
+	rx, err := regexp.Compile(`(?:[\(\)]|[\s][^a-zA-Z]+)`)
+	if err != nil {
+		return nil, err
+	}
+	stringRemove := rx.ReplaceAllString(line, "")
+	genders := strings.Split(stringRemove, "|")
+	if err != nil {
+		return nil, err
+	}
+	return genders, nil
+}
+
+func channalOfGenders(genders []string, g chan string) {
+	var gender string
+	for i := range genders {
+		//gender = genders[i]
+		log.Printf("gender: %v\n", genders[i])
+	}
+	g <- gender
 }
